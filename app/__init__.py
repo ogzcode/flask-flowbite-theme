999933860b2f80987033379021279fbc545686ca
ogzCode <oguzguc44@gmail.com>
2024-08-23 12:58:36 +0300

    feat: Add Flask app initialization and basic routes
    This commit adds the necessary code to initialize the Flask app, including setting up the database, CORS, and login manager. It also registers the blueprint for authentication routes and adds a basic index route. These changes enhance the project by providing a solid foundation for the app's functionality and structure.

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_cors import CORS
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login' 
+
+from app.routes import auth_routes
+
+app.register_blueprint(auth_routes)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
