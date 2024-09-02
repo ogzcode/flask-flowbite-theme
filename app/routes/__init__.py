ad415967fc3d1d661e8d59f42d4389f2fed7d3a3
ogzCode <oguzguc44@gmail.com>
2024-09-02 17:48:44 +0300

    feat: Add e-commerce routes and API endpoint for product list data

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .auth import auth_routes
 from .dashboard import dashboard_routes
+from .e_commerce import e_commerce_routes
---
+++ app/routes/e_commerce.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, render_template, jsonify, request
+from flask_login import login_required
+from faker import Faker
+from app.models import Product
+
+e_commerce_routes = Blueprint('e_commerce_routes', __name__)
+
+@e_commerce_routes.route('/e-commerce/product-list', methods=['GET'])
+@login_required
+def product_list():
+    return render_template('pages/e-commerce/product-list.html')
+
+
+@e_commerce_routes.route('/e-commerce/product-list/data', methods=['GET'])
+@login_required
+def product_list_data():
+    PER_PAGE = 10
+    search = request.args.get('search')
+    sort_by = request.args.get('sort_by', "id")
+    sort_order = request.args.get('sort_order', "asc")
+    page = int(request.args.get('page', 1))
+    start = (page - 1) * PER_PAGE
+    end = start + PER_PAGE
+
+    products = Product.query
+
+    if search:
+        products = products.filter(Product.name.ilike(f"%{search}%"))
+    
+    if sort_order == "asc":
+        products = products.order_by(getattr(Product, sort_by).asc())
+
+    if sort_order == "desc":
+        products = products.order_by(getattr(Product, sort_by).desc())
+
+    products = products.slice(start, end)
+
+    data = []
+
+    for product in products:
+        data.append({
+            'id': product.id,
+            'name': product.name,
+            'description': product.description,
+            'price': product.price,
+            'stock': product.stock,
+            'category': product.category,
+        })
+
+    return jsonify({
+        'data': data,
+        'total': Product.query.count(),
+        'page': page,
+        "from": start,
+        "to": end,
+    })
